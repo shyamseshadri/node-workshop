81201fb930697f00ac8b889785375d30871e7c7e
Shyam Seshadri <shyamseshadri@gmail.com>
2019-06-19 15:44:39 +0530

    S2-6: Working login api reading post

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 const teamCtrl = require('./team/team.controller');
 
-app.get('/', (req, res) => {
+app.use(express.static(__dirname + '/public'));
+
+app.use(bodyParser.json())
+
+app.get('/api/teams', (req, res) => {
   teamCtrl.getTeams((err, teams) => {
     res.status(200).json(teams);
   });
+});
+
+app.post('/api/login', (req, res) => {
+  var username = req.body.username;
+  var password = req.body.password;
+  if (username === 'admin' && password === 'password') {
+    res.json({msg: 'Successful Login'});
+  } else {
+    res.status(400).send({msg: 'FAILED Login'});
+  }
 });
 
 app.listen(3000, () => {
